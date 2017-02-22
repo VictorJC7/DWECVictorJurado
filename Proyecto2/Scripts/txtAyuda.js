3fec8326712fc98b2c87c276dee5bddd2d74b8da
VictorJC7 <victorjc7@gmail.com>
2017-02-22 01:25:48 +0100

    djsh

--- Proyecto2/Scripts/txtAyuda.js
+++ Proyecto2/Scripts/txtAyuda.js
@@ -3,7 +3,7 @@ function mostrarAyuda(e) {
   e.preventDefault();
   var dondeVaAyuda = $('.enseñaAyuda');
   $.getJSON('ayuda.json', function (data) {
-    $('.ensenaAyuda').html(
+    $('.enseñaAyuda').html(
       "Nombre: " + data.Nombre.text + "<br>" +
       "Telefono: " + data.Telefono.numero + "<br>" +
       "Email: " + data.Email.email + "<br>" +
