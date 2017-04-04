edac70b540d55aebcf0e6d3d2bec328e1761c859
victor jurado clemente <victor jurado clemente>
2017-04-04 20:18:21 +0200

    djsh

--- Proyecto/Paginas/ficha.php
+++ Proyecto/Paginas/ficha.php
@@ -8,35 +8,6 @@
     <link rel=stylesheet href="../CSS/style.css" type="text/css">
   </head>
   <body>
-    <h1>Prueba</h1>
-    <div class="container-fluid">
-      <div class="tabla col-lg-12 col-ms-12 col-sm-12 col-xs-12">
-        <div class="rTableRow">
-          Nombre del producto: <?php echo $consultaNombreProducto; ?>.
-        </div>
-        <div class="rTableRow">
-          Descripcion: <?php echo $consultaDescripcionProducto; ?>.
-        </div>
-        <div class="rTableRow">
-          Precio: <?php echo $consulaPrecioProducto; ?>.
-        </div>
-        <div class="rTableRow">
-          <!-- Codigo QR -->
-          <img src="<?php echo $consultaSrcImagen; ?>" alt="Imagen del producto <?php echo $consultaNombreProducto; ?>" width="500px" height="500px">
-        </div>
-          <?php
-            if (!empty($URLVideo)) {
-              echo "<div class='TableRow'>
-              <a href='" + $URLVideo; + "' target='_blank'>Ver video sobre instruccioes de aplicado</a></div>";
-            }
-           ?>
-           <div class="rTableRow">
-             Categoria: <?php echo $consulaCategoriaProducto; ?>.
-             <a href="#">Mas información acerca de la categoria.</a>
-           </div>
-           <div class="rTableRow">
-             Proveedor: <?php echo $consulaProveedorProducto; ?>.
-           </div>
       </div>
     </div>
   </body>
